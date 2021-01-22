12c8ee3f047bc69976b82d6f2ecb2d5dfed5ec0a
Istvan Barabasi <stephan@MBP.local>
2021-01-22 00:50:19 -0500

    First Commit

---
+++ server/public/javascripts/index.js
@@ -0,0 +1,40 @@
+(function() {
+
+    $('.answers').hide();
+    $('.loading').hide();
+    $('#form').submit(onFormSubmit);
+    $('#SquareFeet').val('2000');
+    $('#Bedrooms').val('3');
+
+    function onFormSubmit() {
+        var age = $('#SquareFeet').val();
+        var gender = $('#Bedrooms').val();
+        $('.loading').show();
+        $('.answers').hide();
+        $('.classify-btn').prop('disabled', true);
+        $.post("/classify", {SquareFeet: SquareFeet,
+                             Bedrooms: Bedrooms
+                            }, function(data) {
+            renderAnswer(data)
+        }).fail(function(err) {
+            renderAnswer(err);
+        });
+        return false;
+    }
+
+    function renderAnswer(parsedResponse) {
+        console.log(parsedResponse);
+
+        if (parsedResponse.errors) {
+            $('.answer').html('Something went wrong :-( ' + parsedResponse.errors[0].message);
+        } else {
+            var data = parsedResponse.predictions[0].values[0]
+            var prediction = data[3][0]
+            $('.prediction').html('Prediction: prediction);
+        }
+
+        $('.classify-btn').prop('disabled', false);
+        $('.answers').show();
+        $('.loading').hide();
+    }
+}());
